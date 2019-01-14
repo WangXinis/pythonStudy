e7fdb79d57a0115ea3ae21076edc08e82f720950
SinWang <wangxinel2016@163.com>
2019-01-14 17:32:37 +0800

    添加了贪婪模式和懒惰模式

--- test.py
+++ test.py
@@ -39,4 +39,33 @@ print(result)
     U 根据Unicode字符解析字符
     S 让.可以匹配换行符，就是用了这个之后.可以匹配任意字符了
 """
-
+print("*"*50,"这里是模式修正的案例")
+pattern1="python"
+pattern2="python"
+string="adfasdfsaPython_py"
+result1=re.search(pattern1,string)
+result2=re.search(pattern2,string,re.I)
+print(result1)
+print(result2)
+pattern3=".*"
+string="""
+    www.baidu.com
+    www.sina.com
+"""
+result3=re.search(pattern3,string,re.S)
+result4=re.search(pattern3 ,string,re.UNICODE)
+print(result3)
+print(result4)
+"""
+    贪婪模式与懒惰模式
+   贪婪模式：尽可能多的匹配；
+   懒惰模式：尽可能少的匹配 
+"""
+print("*"*20,"贪婪模式与懒惰模式匹配例子","*"*20)
+patter1="py.*y"#贪婪模式
+pattern2="p.*?y"#懒惰模式
+string="abcdpython_xyyy"
+result1=re.search(pattern1,string)
+result2=re.search(pattern2,string)
+print(result1)
+print(result2)
