38b8d7d9bb72587df53e607bb274b6969d5d8df7
Wangxin1708 <wangxinel2016@163.com>
2024-02-21 22:11:16 +0800

    2024年工作后第一次提交

--- mywebstudy/myproject/boards/views.py
+++ mywebstudy/myproject/boards/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from boards.models import Board
 # Create your views here.
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+   #boards = Board.objects.all()
+    #boards_names = list()
+
+    #for board in boards:
+     #   boards_names.append(board.name)
+
+    #response_html = '<br>'.join(boards_names)
+
+    #return HttpResponse(response_html)
+    
--- mywebstudy/myproject/myproject/urls.py
+++ mywebstudy/myproject/myproject/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import re_path as url
 from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
+from boards import views
+urlpatterns =[
+    url(r'^$', views.home,name="home"),
+    url("",views.index,name="index"),
+    url(r'^admin/', admin.site.urls),
 ]
