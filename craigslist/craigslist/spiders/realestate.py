61eab2b5df3c967662072d732806028b3805ee90
SinWang <914606593@qq.com>
2024-03-01 18:24:57 +0800

    20240301

---
+++ craigslist/craigslist/spiders/realestate.py
@@ -0,0 +1,31 @@
+import scrapy
+
+
+class RealestateSpider(scrapy.Spider):
+    name = "realestate"
+    allowed_domains = ["newyork.craigslist.org"]
+    start_urls = ["https://newyork.craigslist.org/d/real-estate/search/rea"]
+    """def parse(self, response):
+        allAds = response.css("p.result-info")
+
+        for ad in allAds:
+            date = ad.css("time::text").get()
+            title =  ad.css("a.result-title.hdrlnk::text").get()
+            price = ad.css("span.result-price::text").get()
+            link = ad.css("a::attr(href)").get()
+
+            print("====NEW PROPERTY===")
+            print(date)
+            print(title)
+            print(price)
+            print(link)
+
+            print("\n")
+    
+        """
+    def parse(self, response):
+        print("\n")
+        print("HTTP STATUS: "+str(response.status))
+        print(response.css("title::text").get())
+        print("\n")
+    
