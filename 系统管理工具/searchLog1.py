09d7c71723281786d0dc8fde8f9755e94a37d2aa
SinWang <914606593@qq.com>
2024-11-19 12:03:16 +0800

    处理ip日志

--- 系统管理工具/searchLog1.py
+++ 系统管理工具/searchLog1.py
@@ -6,16 +6,29 @@ import re
 
 # 正则表达式模式
 #pattern = r'(\b(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\b \d{1,2} \d{2}:\d{2}:\d{2})\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-pattern = r'(\(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\b \d{1,2} \d{2}:\d{2}:\d{2})\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+#pattern = r'(\(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\b \d{1,2} \d{2}:\d{2}:\d{2})\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+pattern = r'(\b(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\b \d{1,2} \d{2}:\d{2}:\d{2})\s+(.*\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+ip_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
 #pattern = r'^(\d{4}-\d{2}-\d{2})(.+)((?:\d{1,3}.){3}\d{1,3})$'
 # 读取文件内容
 with open('example.txt', 'r') as file:
-        content = file.readline()
+        content = file.read()
         print(content)
-
         # 查找所有匹配的日期和IP地址
         matches = re.findall(pattern, content)
-        print("matches:",matches)
+        #ip_matches=re.findall(ip_pattern,content)
+        
+        #print("matches:",matches)
         # 打印所有找到的日期和IP地址
+        """
+        ip_match = re.search(ip_pattern, text)
+        ip = ip_match.group()
+        """
         for match in matches:
-                print(f"Date: {match[0]}, IP: {match[1]}")
+                #匹配日期
+                #print(f"Date: {match[0]}, IP: {match[1]}",end=",")
+                print(f"Date: {match[0]}",end=",")
+                #匹配ip地址
+                ip_match=re.search(ip_pattern, str(match[1]))
+                IP=ip_match.group()
+                print(f"IP:{IP}")
--- 系统管理工具/test.py
+++ 系统管理工具/test.py
@@ -5,11 +5,11 @@ text = "Nov 13 12:25:05 hecs-395634 sshd[488276]: pam_unix(sshd:auth): authentic
 
 # 正则表达式模式
 pattern = r'(\b(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\b \d{1,2} \d{2}:\d{2}:\d{2})\s+(.*\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-
+ip_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
 # 查找匹配的日期和IP地址
 match = re.search(pattern, text)
-
+ip_match = re.search(ip_pattern, text)
 if match:
     date = match.group(1)
-    ip = match.group(2)
+    ip = ip_match.group()
     print(f"Date: {date}, IP: {ip}")
