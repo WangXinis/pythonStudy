708a80742c148c328e8c7130de0c0d1013261b35
WangXinis <31101495+WangXinis@users.noreply.github.com>
2024-11-22 15:07:47 +0800

    python 学习手册665页

--- SelfStudyForPython/person.py
+++ SelfStudyForPython/person.py
@@ -21,6 +21,8 @@ print(bob.name,bob.pay)
 print(sum.name,sum.pay)
 """
 class Manager(Person):
+    def __init__(self,name,pay=0):
+        Person.__init__(self,name,'mgr',pay)
     def giveRaise(self,percent,bonus=.10):
         #self.pay=int(self.pay*(1+percent+bonus))
         Person.giveRaise(self,percent+bonus)
@@ -35,7 +37,13 @@ if __name__=="__main__":
     print(bob.pay)
     print(sum,bob)
 
-    tom=Manager('Tom Doe','mgr',50000)
+    #tom=Manager('Tom Doe','mgr',50000)
+    tom=Manager('Tom Doe',50000)
     tom.giveRaise(.10)
     print(tom.lastanme(),tom.pay)
     print(tom)
+
+    print("------all Tree---")
+    for obj in (bob,sum,tom):
+        obj.giveRaise(.10)
+        print(obj)
